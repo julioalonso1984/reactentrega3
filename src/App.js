505f8d7def306775631a0e4fa9011734f732f726
julioalonso1984 <julsalonso@hotmail.com>
2022-08-31 21:10:47 -0300

    master

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import ItemListContainer from './Components/Navbar/ItemListContainer';
 import Navbar from './Components/Navbar/Navbar';
 
 function App() {
-  //const productos = ["Heladera", "Computadora", "Lavavajillas"]
+  
   const productos = [
     {id:1, producto:"Heladera", precio:90000},
-     {id:2, producto:"Computadora", precio:150000},
+    {id:2, producto:"Computadora", precio:150000},
     {id:3, producto:"Lavajillas", precio:160000}
     ]
-  //const productos = ["Heladera", "Computadora", "Lavajillas"];
+  
   const items = productos.map(items => (
     <ItemListContainer valor={items} />
   ))
@@ -21,7 +21,7 @@ function App() {
       <ul>
         {items}
       </ul>
-      {/* <ItemListContainer items = {productos} titulo="productos en venta" /> */}
+      
     </div>
   );
 }
